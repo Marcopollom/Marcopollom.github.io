a7a72012dfa37b930d66cafd56673f80657caf9e
Rohan Simon <rohan.simon@ufl.edu>
2022-08-16 17:33:02 -0400

    rewrote redirect page using svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+
+	// redirect user to main page if they visit any other page
+	import { page } from '$app/stores';
+	import { redirect } from '@sveltejs/kit';
+	import { onMount } from 'svelte';
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<meta http-equiv="refresh" content="2; URL=https://rrohans.github.io/" />
+	<title>Redirecting</title>
+</svelte:head>
+
+<div class="flex justify-center text-2xl pt-10">
+	<h1>
+		Page has moved! Redirecting to <a
+			class="underline text-blue-700"
+			href="https://rrohans.github.io/">https://rrohans.github.io/</a
+		>
+	</h1>
+</div>
